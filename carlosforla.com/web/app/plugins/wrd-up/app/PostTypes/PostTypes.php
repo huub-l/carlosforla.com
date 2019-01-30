252c7b7cca6186cca1e96be1c7c711f35d30a354
Kelly Mears <code@kellymears.me>
2019-01-30 09:09:38 -0800

    FIX: action for prevous commit

--- carlosforla.com/web/app/plugins/wrd-up/app/PostTypes/PostTypes.php
+++ carlosforla.com/web/app/plugins/wrd-up/app/PostTypes/PostTypes.php
@@ -16,12 +16,14 @@ $types = new Taxonomy('type');
 $types->options(['hierarchical' => false]);
 $types->register();
 
-acf_add_local_field_group(
-    array(
-    'key' => 'group_5c50d8d402f57',
-    'title' => 'Event',
-    'fields' => array(
-        array(
+add_action(
+    'acf/init', function () {
+        acf_add_local_field_group(
+            array(
+            'key' => 'group_5c50d8d402f57',
+            'title' => 'Event',
+            'fields' => array(
+            array(
             'key' => 'field_5c50d8ec936f1',
             'label' => 'Location',
             'name' => 'location',
@@ -30,16 +32,16 @@ acf_add_local_field_group(
             'required' => 0,
             'conditional_logic' => 0,
             'wrapper' => array(
-                'width' => '',
-                'class' => '',
-                'id' => '',
+            'width' => '',
+            'class' => '',
+            'id' => '',
             ),
             'center_lat' => '',
             'center_lng' => '',
             'zoom' => '',
             'height' => '',
-        ),
-        array(
+            ),
+            array(
             'key' => 'field_5c50d909936f2',
             'label' => 'Street',
             'name' => 'street',
@@ -48,17 +50,17 @@ acf_add_local_field_group(
             'required' => 0,
             'conditional_logic' => 0,
             'wrapper' => array(
-                'width' => '',
-                'class' => '',
-                'id' => '',
+            'width' => '',
+            'class' => '',
+            'id' => '',
             ),
             'default_value' => '',
             'placeholder' => '',
             'prepend' => '',
             'append' => '',
             'maxlength' => '',
-        ),
-        array(
+            ),
+            array(
             'key' => 'field_5c50d927936f3',
             'label' => 'City',
             'name' => 'city',
@@ -67,17 +69,17 @@ acf_add_local_field_group(
             'required' => 0,
             'conditional_logic' => 0,
             'wrapper' => array(
-                'width' => '',
-                'class' => '',
-                'id' => '',
+            'width' => '',
+            'class' => '',
+            'id' => '',
             ),
             'default_value' => '',
             'placeholder' => '',
             'prepend' => '',
             'append' => '',
             'maxlength' => '',
-        ),
-        array(
+            ),
+            array(
             'key' => 'field_5c50d92f936f4',
             'label' => 'State',
             'name' => 'state',
@@ -86,17 +88,17 @@ acf_add_local_field_group(
             'required' => 0,
             'conditional_logic' => 0,
             'wrapper' => array(
-                'width' => '',
-                'class' => '',
-                'id' => '',
+            'width' => '',
+            'class' => '',
+            'id' => '',
             ),
             'default_value' => '',
             'placeholder' => '',
             'prepend' => '',
             'append' => '',
             'maxlength' => '',
-        ),
-        array(
+            ),
+            array(
             'key' => 'field_5c50d933936f5',
             'label' => 'Zip Code',
             'name' => 'zip_code',
@@ -105,9 +107,9 @@ acf_add_local_field_group(
             'required' => 0,
             'conditional_logic' => 0,
             'wrapper' => array(
-                'width' => '',
-                'class' => '',
-                'id' => '',
+            'width' => '',
+            'class' => '',
+            'id' => '',
             ),
             'default_value' => '',
             'placeholder' => '',
@@ -116,8 +118,8 @@ acf_add_local_field_group(
             'min' => '',
             'max' => '',
             'step' => '',
-        ),
-        array(
+            ),
+            array(
             'key' => 'field_5c50d943936f6',
             'label' => 'Date',
             'name' => 'time',
@@ -126,15 +128,15 @@ acf_add_local_field_group(
             'required' => 0,
             'conditional_logic' => 0,
             'wrapper' => array(
-                'width' => '',
-                'class' => '',
-                'id' => '',
+            'width' => '',
+            'class' => '',
+            'id' => '',
             ),
             'display_format' => 'd/m/Y g:i a',
             'return_format' => 'd/m/Y g:i a',
             'first_day' => 1,
-        ),
-        array(
+            ),
+            array(
             'key' => 'field_5c51d84c74b83',
             'label' => 'Event Embed Code',
             'name' => 'event_embed',
@@ -143,45 +145,47 @@ acf_add_local_field_group(
             'required' => 0,
             'conditional_logic' => 0,
             'wrapper' => array(
-                'width' => '',
-                'class' => '',
-                'id' => '',
+            'width' => '',
+            'class' => '',
+            'id' => '',
             ),
             'default_value' => '',
             'placeholder' => '',
             'maxlength' => '',
             'rows' => '',
             'new_lines' => '',
-        ),
-    ),
-    'location' => array(
-        array(
-            array(
-                'param' => 'post_type',
-                'operator' => '==',
-                'value' => 'post',
             ),
-        ),
-    ),
-    'menu_order' => 0,
-    'position' => 'acf_after_title',
-    'style' => 'seamless',
-    'label_placement' => 'top',
-    'instruction_placement' => 'label',
-    'hide_on_screen' => array(
-        0 => 'discussion',
-        1 => 'comments',
-        2 => 'revisions',
-        3 => 'slug',
-        4 => 'author',
-        5 => 'format',
-        6 => 'page_attributes',
-        7 => 'featured_image',
-        8 => 'categories',
-        9 => 'tags',
-        10 => 'send-trackbacks',
-    ),
-    'active' => 1,
-    'description' => '',
-    )
+            ),
+            'location' => array(
+            array(
+            array(
+            'param' => 'post_type',
+            'operator' => '==',
+            'value' => 'post',
+            ),
+            ),
+            ),
+            'menu_order' => 0,
+            'position' => 'acf_after_title',
+            'style' => 'seamless',
+            'label_placement' => 'top',
+            'instruction_placement' => 'label',
+            'hide_on_screen' => array(
+            0 => 'discussion',
+            1 => 'comments',
+            2 => 'revisions',
+            3 => 'slug',
+            4 => 'author',
+            5 => 'format',
+            6 => 'page_attributes',
+            7 => 'featured_image',
+            8 => 'categories',
+            9 => 'tags',
+            10 => 'send-trackbacks',
+            ),
+            'active' => 1,
+            'description' => '',
+            )
+        );
+    }
 );
